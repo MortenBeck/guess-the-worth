12948f265c8f6c171ccf8848719e52bb2d42063c
s204176 <s204176@student.dtu.dk>
2025-09-25 16:23:47 +0200

    Add favorites store and update artwork/favorites pages

---
+++ frontend/src/store/favoritesStore.js
@@ -0,0 +1,35 @@
+import { create } from 'zustand'
+
+const useFavoritesStore = create((set, get) => ({
+  favorites: [],
+
+  addToFavorites: (artwork) => {
+    const currentFavorites = get().favorites
+    if (!currentFavorites.find(fav => fav.id === artwork.id)) {
+      set({
+        favorites: [...currentFavorites, { ...artwork, dateAdded: new Date().toISOString() }]
+      })
+    }
+  },
+
+  removeFromFavorites: (artworkId) => {
+    set({
+      favorites: get().favorites.filter(fav => fav.id !== artworkId)
+    })
+  },
+
+  isFavorite: (artworkId) => {
+    return get().favorites.some(fav => fav.id === artworkId)
+  },
+
+  toggleFavorite: (artwork) => {
+    const isFav = get().isFavorite(artwork.id)
+    if (isFav) {
+      get().removeFromFavorites(artwork.id)
+    } else {
+      get().addToFavorites(artwork)
+    }
+  }
+}))
+
+export default useFavoritesStore
